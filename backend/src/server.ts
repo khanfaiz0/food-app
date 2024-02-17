ddf924cfc11ba0cc3914f6682c8721414f993eda
Faizan <122879780+khanfaiz0@users.noreply.github.com>
2024-02-17 12:25:14 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json())
 app.use(cors({
     credentials:true,
-    origin:["http://localhost:4200"]
+    origin: '*',
 }));
 
 app.use("/api/foods",foodRouter)
